ca493a047c294413ec0ed63ed71bb73ddba05897
Weston Hanners <weston@hanners.us>
2025-12-18 08:46:09 -0500

    Added home manager

---
+++ home.nix
@@ -0,0 +1,26 @@
+{ config, pkgs, ... }:
+
+{
+  home.username = "weston";
+  home.homeDirectory = "/home/weston";
+
+  programs.swaylock.enable = true; # Super+Alt+L in the default setting (screen locker)
+  services.mako.enable = true; # notification daemon
+  services.swayidle.enable = true; # idle management daemon
+  services.polkit-gnome.enable = true; # polkit
+
+  home.packages = with pkgs; [
+    alacritty
+    fuzzel
+    swaybg
+    steamtinkerlaunch
+    wget
+    xdotool
+    unzip
+    xxd
+    yad
+    xwininfo
+  ];
+
+  home.stateVersion = "25.11";
+}
